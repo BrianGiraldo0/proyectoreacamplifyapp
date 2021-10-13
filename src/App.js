e2dcf1497b99dd408c56158cfaaad916088d7d97
Brian Giraldo <briand.giraldop@uqvirtual.edu.co>
2021-10-13 08:34:05 -0500

    modificacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cambios del curso Diurno
         </p>
         <a
           className="App-link"
